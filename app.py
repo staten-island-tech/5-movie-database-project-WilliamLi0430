0408130f44f582c9bcdea49c29b423582c24d7ac
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-11-07 09:25:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,19 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the entire movie list
 data = json.load(movies)
-for index, item in enumerate(data):
-    print(item["title"])
+print("What category are you searching up? ")
+searchbar = input("Categories: title, year, cast, genres, href, extract, thumbnail, thumbnail_width, thumbnail_height. ")
+for i in data:
+    if searchbar.lower() in i["title"].lower():
+        print(f"{i["title"].lower()} ")
+    else:
+        print(f"{i["title"].lower()} ")
+
+
+
+
+
+
+#input("Categories: title, year, cast, genres, href, extract, thumbnail, thumbnail_width, thumbnail_height. ")
+#for index, item in enumerate(data):
+    #print(item["title"])
