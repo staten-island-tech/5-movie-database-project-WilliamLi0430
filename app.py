c11b107e0520033b6090b2ee08e295c412a4f21a
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-11-10 09:25:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the entire movie list
 data = json.load(movies)
-print("What category are you searching up? ")
-searchbar = input("Categories: title, year, cast, genres, href, extract, thumbnail, thumbnail_width, thumbnail_height. ")
-for i in data:
-    if searchbar.lower() in i["title"].lower():
-        print(f"{i["title"].lower()} ")
-    else:
-        print(f"{i["title"].lower()} ")
+searchbar = int(input("What"))
+if int("year") > searchbar:
+    print("title")
 
 
 
