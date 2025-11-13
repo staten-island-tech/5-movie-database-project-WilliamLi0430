a0144608eb5f63217bf344bae5a87c1b5ad5a823
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-11-13 09:25:49 -0500

    e

--- furo.py
+++ furo.py
@@ -1,7 +1,7 @@
 import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
-variable = int(input("what"))
+variable = int(input("what "))
 for index, item in enumerate(data):
     if item["year"] == variable:
         print(item["title"])
--- wu.py
+++ wu.py
@@ -1,3 +1,7 @@
 import json
 movies = open("./movies.json", encoding="utf8")
 data = json.load(movies)
+variable = input("what ")
+for index, item in enumerate(data):
+    if item["title"] == variable:
+        print(item["title"])
